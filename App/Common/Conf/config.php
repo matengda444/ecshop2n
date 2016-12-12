1eb7a1f7425901c4ed9f8d35e7a0684c421f0fcc
matengda444 <matengda444@163.com>
2016-12-13 04:20:12 +0800

    add config.php

---
+++ App/Common/Conf/config.php
@@ -0,0 +1,14 @@
+<?php
+return array(
+	//'配置项'=>'配置值'
+	'DB_TYPE'               =>  'mysql',     // 数据库类型
+    'DB_HOST'               =>  '127.0.0.1', // 服务器地址
+    'DB_NAME'               =>  'e2',          // 数据库名
+    'DB_USER'               =>  'root',      // 用户名
+    'DB_PWD'                =>  'root',          // 密码
+    'DB_PORT'               =>  '3306',        // 端口
+    'DB_PREFIX'             =>  'e2',    // 数据库表前缀
+	'URL_MODEL'             =>  2,       // URL访问模式,可选参数0、1、2、3,代表以下四种模式：
+    // 0 (普通模式); 1 (PATHINFO 模式); 2 (REWRITE  模式); 3 (兼容模式)  默认为PATHINFO 模式
+    'URL_PATHINFO_DEPR'     =>  '-',	// PATHINFO模式下，各参数之间的分割符
+);
