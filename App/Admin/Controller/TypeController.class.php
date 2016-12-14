3b66b4237083934b97c442d7d516e279aac98f66
matengda444 <matengda444@163.com>
2016-12-15 05:34:35 +0800

    update TypeController.class.php

--- App/Admin/Controller/TypeController.class.php
+++ App/Admin/Controller/TypeController.class.php
@@ -3,10 +3,23 @@ namespace Admin\Controller;
 
 use Think\controller;
 
-class TypeController extends Controller
+class TypeController extends Controller //添加商品类型
 {
-    public function add()
-    {
-        $this->display();
+    public function add() {
+        if (IS_POST) {
+            $typemodel = D('Type');
+            if ($typemodel->create(I('post'), 1)) { //在创建数据验证，在创建数据验证时，完成自动验证功能。
+            //create($data = '', $type = '')$type=1是执行插入操作,$type=2是执行更新操作,如果不传值,则是自己分析是插入还是更新
+                if ($typemodel->add()) {
+                    $this->success('添加成功', U('lst'));
+                    exit;
+                } else {
+                    $this->error('添加失败');
+                }
+            } else {
+                $this->error($typemodel->getError());
+            }
+        }
+    $this->display(); //输出验证失败后的错误提示信息。
     }
 }
