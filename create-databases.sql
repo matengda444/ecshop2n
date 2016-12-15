769c1a7cbb5e8f47c3ac17a7b3bafb86e6bae082
matengda444 <matengda444@163.com>
2016-12-15 19:11:49 +0800

    update create-databases.sql

--- create-databases.sql
+++ create-databases.sql
@@ -4,3 +4,13 @@ create table e2_type(
     index (type_name)
 )engine myisam charset utf8;
 --创建一个商品类型表
+create table e2_attribute(
+    id smallint unsigned primary key auto_increment,
+    type_id tinyint unsigned not null comment '商品类型表id',
+    attr_name varchar(32) not null comment '属性名称',
+    attr_type tinyint not null comment '属性类型 0唯一属性,1列表选择',
+    attr_input_type tinyint not null comment '属性值录入方式 0手工, 1列表选择',
+    attr_value varchar(64) not null default '' comment '属性列表选择中的值',
+    index(type_id)
+)engine myisam charset utf8;
+--创建一个商品属性表
