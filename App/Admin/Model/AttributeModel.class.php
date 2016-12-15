ca9354f1365759bc671e75d3a06507f2dd49137d
matengda444 <matengda444@163.com>
2016-12-15 22:09:01 +0800

    update AttributeModel.class.php

--- App/Admin/Model/AttributeModel.class.php
+++ App/Admin/Model/AttributeModel.class.php
@@ -6,6 +6,7 @@ use Think\Model;
 class AttributeModel extends Model
 {
     protected $_validate = array(
+        //array(验证字段1,验证规则,错误提示,[验证条件,附加规则,验证时间])
         array('attr_name', 'require', '属性名称不能为空'),
         array('type_id', 'isGt0', '商品类型不合法', 1, callback), //callback表示要使用当前模型类的一个方法
         array('attr_type', array(0,1), '属性类型不合法', 1, 'in'),
