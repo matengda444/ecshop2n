0e09fd0fd6a40b885add9a538a6f0e7afb49da99
matengda444 <matengda444@163.com>
2016-12-17 00:35:10 +0800

    update CategoryController.class.php

--- App/Admin/Controller/CategoryController.class.php
+++ App/Admin/Controller/CategoryController.class.php
@@ -7,6 +7,27 @@ class CategoryController extends Controller
 {
     public function add()
     {
+        $catemodel = D('Category');
+        if (IS_POST) {
+            if ($catemodel->create()) {
+                if ($catemodel->add()) {
+                    $this->success('添加成功', U('lst'));
+                    exit;
+                } else {
+                    $this->error('添加失败');
+                }
+            } else {
+                $this->error($catemodel->getError());
+            }
+        }
+        $catedata = $catemodel->getTree();
+        $this->assign('catedata', $catedata);
+        $this->display();
+    }
+    public function lst() {
+        $catemodel = D('Category');
+        $catedata = $catemodel->getTree();
+        $this->assign('catedata', $catedata);
         $this->display();
     }
 }
