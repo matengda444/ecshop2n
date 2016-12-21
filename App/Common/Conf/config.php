83415ca4c4d45c6496629d6bc705aaf2415a5abb
matengda444 <matengda444@163.com>
2016-12-21 18:23:08 +0800

    update config.php

--- App/Common/Conf/config.php
+++ App/Common/Conf/config.php
@@ -11,4 +11,7 @@ return array(
 	'URL_MODEL'             =>  2,       // URL访问模式,可选参数0、1、2、3,代表以下四种模式：
     // 0 (普通模式); 1 (PATHINFO 模式); 2 (REWRITE  模式); 3 (兼容模式)  默认为PATHINFO 模式
     'URL_PATHINFO_DEPR'     =>  '-',	// PATHINFO模式下，各参数之间的分割符
+    'UPLOAD_ROOT_PATH'      =>  './Public/Uploads/',    //上传文件根路径   (.)基于入口文件位置
+    'UPLOAD_MAX_FILESIZE'   =>  '10M',   //上传文件最大值
+    'UPLOAD_ALLOW_EXT'      =>  array('jpeg', 'jpg', 'png', 'gif', 'bmp'),    //上传文件类型
 );
