0bc85265a12e6a356afa3ba92849229e7ec653e9
matengda444 <matengda444@163.com>
2016-12-25 17:55:03 +0800

    update AdminModel.class.php

--- App/Admin/Model/AdminModel.class.php
+++ App/Admin/Model/AdminModel.class.php
@@ -15,6 +15,14 @@ class AdminModel extends Model
     public $_login_validate = array(
         array('admin_name', 'require', '管理员名称不能为空'), //$_login_validate 该名称任意
         array('password', 'require', '密码不能为空'),
-        array('checkcode', 'require', '验证码不能为空')
+        array('checkcode', 'require', '验证码不能为空'),
+        array('checkcode', 'check_verify', '验证码不能为空', 1, 'callback') //check_verify和下方的check_verify对应,
+        //callback表示该条规则要使用当前类的一个方法来完成验证
     );
+    protected function check_verify($code, $id = '')
+    {
+        //验证,验证码
+        $verify = new \Think\Verify();
+        return $verify->check($code, $id);
+    }
 }
