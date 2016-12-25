0c68740e9e411aae1bba12353fda2cc7b5583239
matengda444 <matengda444@163.com>
2016-12-25 14:37:37 +0800

    update LoginController.class.php

--- App/Admin/Controller/LoginController.class.php
+++ App/Admin/Controller/LoginController.class.php
@@ -5,7 +5,22 @@ use Think\Controller;
 
 class LoginController extends Controller
 {
-    public function login() {
+    //显示登陆页面
+    public function login()
+    {
         $this->display();
+    }
+    public function authcode()
+    {
+        //生成验证码的方法
+        $config = array(
+            'fontSize' => 20, //验证码字体大小
+            'length' => 4, //验证码位数
+            'useNoise' => false, //关闭验证码杂点
+            'imageW' => 140, //验证码宽度
+            'imageH' => 40 //验证码高度
+        );
+        $Verify = new\Think\Verify($config);
+        $Verify->entry();
     }
 }
