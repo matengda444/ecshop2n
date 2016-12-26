d72b5e04f9211f7b4a7c06b96e224696bd93b8d8
matengda444 <matengda444@163.com>
2016-12-27 00:23:08 +0800

    add RoleModel.class.php

---
+++ App/Admin/Model/RoleModel.class.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: MY
+ * Date: 2016/12/27
+ * Time: 0:21
+ */
+namespace Admin\Model;
+
+use Think\Model;
+
+class RoleModel extends Model
+{
+    protected $_validate = array(
+        array('role_name', 'require', '角色名称不能为空')
+    );
+}
