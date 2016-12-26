dc3fd1ba518d64e53209cc5f5fdbb9e947806cac
matengda444 <matengda444@163.com>
2016-12-26 18:00:31 +0800

    add PrivilegeController.class.php

---
+++ App/Admin/Controller/PrivilegeController.class.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: MY
+ * Date: 2016/12/26
+ * Time: 17:58
+ */
+namespace Admin\Controller;
+
+use Think\Controller;
+
+class PrivilegeController extends AuthController
+{
+    public function add()
+    {
+        $this->display();
+    }
+}
