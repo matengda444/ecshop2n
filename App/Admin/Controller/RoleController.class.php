8c0034ed221cfdf47abaa1c9c94215af5b323220
matengda444 <matengda444@163.com>
2016-12-27 20:02:01 +0800

    update RoleModel.class.php

--- App/Admin/Controller/RoleController.class.php
+++ App/Admin/Controller/RoleController.class.php
@@ -33,4 +33,16 @@ class RoleController extends AuthController
         $this->assign('privdata', $privdata);
         $this->display();
     }
+    //角色列表
+    public function lst()
+    {
+        // 取出角色数据并且取出角色对应的权限
+        $rolemodel = D('Role');
+        $roledata = $rolemodel->field("a.*,group_concat(c.priv_name) as privnames")->join("a left join
+       e2_role_privilege b on a.id=b.role_id left join e2_privilege c on b.priv_id=c.id")->group("a.id
+       ")->select();
+        //p($roledata);
+        $this->assign('roledata', $roledata);
+        $this->display();
+    }
 }
