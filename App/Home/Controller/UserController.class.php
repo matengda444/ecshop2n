3514c646f2689537f3c51207a3264ad718e7a6ec
matengda444 <matengda444@163.com>
2017-01-09 23:00:01 +0800

    update UserController.class.php login.html register.html

--- App/Home/Controller/UserController.class.php
+++ App/Home/Controller/UserController.class.php
@@ -5,3 +5,19 @@
  * Date: 2017/1/9
  * Time: 21:33
  */
+namespace Home\Controller;
+
+use Think\Controller;
+
+class UserController extends Controller
+{
+    //注册页面
+    public function register()
+    {
+        $this->display();
+    }
+    public function login()
+    {
+        $this->display();
+    }
+}
