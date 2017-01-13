861ac6e737a962cb0cacf30e340be7295a9a6e80
matengda444 <matengda444@163.com>
2017-01-13 22:22:38 +0800

    Update function.php

--- App/Common/Common/function.php
+++ App/Common/Common/function.php
@@ -40,3 +40,32 @@ function p($a)
     echo '<pre>';
     print_r($a);
 }
+function sendEmail($title, $content, $fromuser, $address)//定义溢恶发送邮件的方法,参数1:信的标题,参数2:信的内容,参数3:署名,参数4:收件人的邮箱
+{
+    //引入邮件发送的类文件
+    require './PHPMailer/class.phpmailer.php';
+    $mail             = new PHPMailer();
+    /*服务器相关信息*/
+    $mail->IsSMTP();                        //设置使用SMTP服务器发送
+    $mail->SMTPAuth   = true;               //开启SMTP认证
+    $mail->Host       = 'smtp.163.com';   	    //设置 SMTP 服务器,自己注册邮箱服务器地址
+    $mail->Username   = 'test';  		//发信人的邮箱用户名
+    $mail->Password   = 'test';          //发信人的邮箱密码
+    /*内容信息*/
+    $mail->IsHTML(true); 			         //指定邮件内容格式为：html
+    $mail->CharSet    ="UTF-8";			     //编码
+    $mail->From       = 't@t.t';	 		 //发件人完整的邮箱名称
+    $mail->FromName   =$fromuser;			 //发信人署名
+    $mail->Subject    = $title;  			 //信的标题
+    $mail->MsgHTML('$content');  				 //发信主体内容
+    $mail->AddAttachment("test.jpg");	     //附件
+    /*发送邮件*/
+    $mail->AddAddress('$address');  			 //收件人地址
+    //使用send函数进行发送
+    if($mail->Send()) {
+        //发送成功返回真
+        return true;
+    } else {
+        return false;
+    }
+}
